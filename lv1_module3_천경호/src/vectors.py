ca22ed65c75048024499f1c3dedf7f5278d32e60
pizzaafterhangover <ckhy2k@gmail.com>
2026-09-07 17:26:45 +0900

    lv1_module3 제출

--- lv1_module3_천경호/src/vectors.py
+++ lv1_module3_천경호/src/vectors.py
@@ -47,7 +47,7 @@ def as_vector(v) -> np.ndarray:
     """
     arr = np.asarray(v, dtype=float)
     if arr.ndim != 1:
-        raise ValueError(f"1차원 벡터가 필요합니다. 받은 shape={arr.shape}")
+        raise ValueError("1차원 벡터가 필요합니다. 받은 shape=", arr.shape)
     return arr
 
 
@@ -99,7 +99,7 @@ def normalize(v, eps: float = 1e-12) -> np.ndarray:
     a = as_vector(v)
     norm_a = norm(a)
     if(norm_a<eps):
-        return a
+        raise ValueError("0벡터는 정규화 할 수 없습니다.")
     return a / norm_a
 
 
@@ -136,7 +136,7 @@ def skew(a) -> np.ndarray:
     # TODO: 문제 1-4
     a = as_vector(a)
     if len(a) != 3:
-        raise ValueError ("3X3만")
+        raise ValueError ("3X3") # 3차원인 경우에 한하여 반대칭 행렬
     return np.array([
         [0,-a[2],a[1]],
         [a[2],0,-a[0]],
@@ -149,7 +149,7 @@ def cross(a, b) -> np.ndarray:
     # TODO: 문제 1-4
     a_skew = skew(a)
     b=as_vector(b)
-    return a_skew@b
+    return a_skew@b  # 반대칭 행렬 곱 리턴
 
 
 def plane_normal(P1, P2, P3) -> np.ndarray:
@@ -159,9 +159,14 @@ def plane_normal(P1, P2, P3) -> np.ndarray:
     세 점이 일직선이면 외적이 영벡터가 되어 평면이 하나로 정해지지 않는다 -> ValueError.
     """
     # TODO: 문제 1-5
-    u=as_vector(P2-P1)
-    v=as_vector(P3-P1)
-    return cross(u,v)
+    P1, P2, P3 = as_vector(P1), as_vector(P2), as_vector(P3)
+    u=P2-P1
+    v=P3-P1
+    pn= cross(u,v)    
+    if np.allclose(pn, 0, atol=1e-12):           # 부동소수점을 감안한 영벡터 검출
+        raise ValueError("세 점이 일직선상에 있습니다.")
+    pn=normalize(pn)    
+    return pn
 
 
 # ------------------------------------------------- 가우스 소거 기반 선형대수
@@ -188,27 +193,31 @@ def row_echelon(A, pivoting: bool = True):
     m, n = a.shape
     n_swaps = 0
     pivot_cols: list[int] = []
-    
-    for i in range(m):
-        if i>=n: break #마지막 열까지 왔다면 멈춤
+    r = 0; # 현재 처리하는 행 인덱스
+    for c in range(n):
+        if c>=n: break #마지막 열까지 왔다면 멈춤
+        if r>=m: break #마지막 행까지 왔다면 멈춤
         #피벗 찾기
-        candidate_row = i+np.argmax(np.abs(a[i:,i]))
-        if np.abs(a[i,candidate_row])<tol: # 피벗이 0에 가까우면 이번 열은 스킵
+        candidate_row = r + np.argmax(np.abs(a[r:,c]))
+        if np.abs(a[candidate_row,c])<tol: # 피벗이 0에 가까우면 이번 열은 스킵
             continue
         
         #스왑
         if pivoting:
-            pivot_cols.append(i)
-            if candidate_row != i:
-                a[[i,candidate_row]]=a[[candidate_row,i]]
-                n_swaps+=1
-            
+            if candidate_row != r:
+                a[[r,candidate_row]]=a[[candidate_row,r]]
+                n_swaps+=1            
+        
+        pivot_cols.append(c) #피벗 열 기록
         
         # 전방소거 - current_row 아래의 모든 행에서 col 열의 원소를 0으로 만들기
-        for j in range(i + 1, m):
-            if np.abs(a[j, i]) > tol:
-                factor = a[j, i] / a[i, i]
-                a[j, i:] -= factor * a[i, i:]
+        for j in range(r + 1, m):
+            if np.abs(a[j, c]) > tol:
+                factor = a[j, c] / a[r, c]
+                a[j, c:] -= factor * a[r, c:]
+                a[j,c] = 0.0 # 부동소수점 오차 삭제
+
+        r += 1 # 소거 완료 후 다음 행으로 이동
     
     return a, pivot_cols, n_swaps
 
@@ -283,60 +292,66 @@ def gauss_eliminate(A, b, pivoting: bool = True, verbose: bool = False):
     b=b[:,np.newaxis]
     agumented = np.hstack((A,b))
 
+    
     # 전방소거 (row_echelon)
     tol = 1e-12
     a = agumented.astype(float, copy=True)
     m, n = a.shape
     n_swaps = 0
-    steps:list=[]
-    
-    for i in range(m):
-        if verbose:
-            steps.append(a.tolist())
-        if i>=n: break #마지막 열까지 왔다면 멈춤
+    steps = []
+    if verbose:
+        steps = a[np.newaxis,:,:]
+        print(f"[초기] 첨가행렬 [A|b]\n{a}")
+
+    r = 0
+    for c in range(n):
+        if c>=n: break #마지막 열까지 왔다면 멈춤
+        if r>=m: break
         #피벗 찾기
-        candidate_row = i+np.argmax(np.abs(a[i:,i]))
-        if np.abs(a[i,candidate_row])<tol: # 피벗이 0에 가까우면 이번 열은 스킵
+        candidate_row = r + np.argmax(np.abs(a[r:,c]))
+        if np.abs(a[candidate_row,c])<tol: # 피벗이 0에 가까우면 이번 열은 스킵
             continue
         
         #스왑
         if pivoting:
-            if candidate_row != i:
-                a[[i,candidate_row]]=a[[candidate_row,i]]
+            if candidate_row != r:
+                a[[r,candidate_row]]=a[[candidate_row,r]]
                 n_swaps+=1
             
-        
         # 전방소거 - current_row 아래의 모든 행에서 col 열의 원소를 0으로 만들기
-        for j in range(i + 1, m):
-            if np.abs(a[j, i]) > tol:
-                factor = a[j, i] / a[i, i]
-                a[j, i:] -= factor * a[i, i:]
+        for j in range(r + 1, m):
+            if np.abs(a[j, c]) > tol:
+                factor = a[j, c] / a[r, c]
+                a[j, c:] -= factor * a[r, c:]
+                a[j,c] = 0.0 #부동소수점 오차 삭제
+        r+=1 #소거 완료 후 다음 행으로 이동
 
-    a=np.asarray(a,dtype=float,copy=True)
+        if verbose:
+            steps = np.concatenate((steps,a[np.newaxis,:,:]), axis=0)
+            print(f"[{c+1}단계]\n{a}")
 
-    tol=1e-12 # 아주 작은 수를 0으로 확인
-    m,n = a.shape
+ 
     rank_A=rank(A)
     rank_agumented=rank(agumented)
 
     if rank_agumented > rank_A:
-        return [] # 모순된 행렬
+        raise ValueError("모순된 행렬")        
     if rank_agumented < n - 1:
-        return [] # 무한 해
+        raise ValueError("무한 해")
     """
     후진대입
     """
     x=np.zeros(n-1)
-    for i in reversed(range(m)):
-        if(a[i]<=tol).sum() == n: #모두 0인 행 거르기
+    for c in reversed(range(m)):
+        if(a[c]<=tol).sum() == n: #모두 0인 행 거르기
             continue
-        x[i] = a[i,n-1]
-        for j in reversed(range(i,n-1)):
-            if i==j:
-                x[i] = x[i] / a[i,j]
+        x[c] = a[c,n-1]
+        for j in reversed(range(c,n-1)):
+            if c==j:
+                x[c] = x[c] / a[c,j]
                 continue
             else:
-                x[i] = x[i] - (a[i,j] * x[j])
+                x[c] = x[c] - (a[c,j] * x[j])
     return x, steps
 
 
@@ -351,7 +366,7 @@ def inverse_gauss_jordan(A) -> np.ndarray:
     m,n = A.shape
     if m != n:
         raise ValueError("정사각 행렬이 아님")
-    argumented = np.hstack(A,np.eye(m))
+    argumented = np.hstack([A,np.eye(m)])
 
     for i in range(n):
         candidate_row = i + np.argmax(np.abs(argumented[i:,i]))
